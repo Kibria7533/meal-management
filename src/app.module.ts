b5aef3bec308304fba2489ae18fbfc1ab9591aa6
golam.kibria@softbdltd.com <kibria7533@A>
2022-07-20 13:22:03 +0600

    dbconnection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BazarListModule } from './bazar-list/bazar-list.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://messdb1:kibria7533@cluster0.ak8uw.mongodb.net/?retryWrites=true&w=majority'),BazarListModule],
   controllers: [AppController],
   providers: [AppService],
 })
