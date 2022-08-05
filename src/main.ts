432512c1874a60a7aa53f1c05bb8d1d5f9e31f76
Rafiul29 <rafiulislam4472@gmail.com>
2022-08-05 10:01:55 +0600

    env added port change

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(5000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
 
