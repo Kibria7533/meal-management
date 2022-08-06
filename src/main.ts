430e4d8836a47893fdf57341a94f6fa3555ee4f6
taskinur rahman <taskinur.rahman2014@gmail.com>
2022-08-06 21:46:00 +0600

    added swagger open api

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 
 
@@ -8,7 +9,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Mess Management System')
+    .setDescription('Mess Management System')
+    .setVersion('1.0')
+    .addTag('Mess')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT);
 }
+
+
 bootstrap();
 
