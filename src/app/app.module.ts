bf6ca65572525228fb7def29648dcea862c0912e
taskinur rahman <taskinur.rahman2014@gmail.com>
2022-08-07 12:49:38 +0600

    remove commented  code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,20 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { BazarListModule } from "../bazar-list/bazar-list.module";
-import { MessModule } from "../mess/mess.module";
-import { DepositModule } from "../deposit/deposit.module";
-import { MemberModule } from "../member/member.module";
-import { MonthlyStatementModule } from "../monthly_statement/monthly_statement.module";
-import { MongooseModule } from "@nestjs/mongoose";
-import { AuthModule } from "../auth/auth.module";
-import { MealEntryModule } from "../meal_entry/meal_entry.module";
-import { RequestModule } from "../request/request.module";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BazarListModule } from '../bazar-list/bazar-list.module';
+import { MessModule } from '../mess/mess.module';
+import { DepositModule } from '../deposit/deposit.module';
+import { MemberModule } from '../member/member.module';
+import { MonthlyStatementModule } from '../monthly_statement/monthly_statement.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from '../auth/auth.module';
+import { MealEntryModule } from '../meal_entry/meal_entry.module';
+import { RequestModule } from '../request/request.module';
 import { ConfigModule } from '@nestjs/config';
-import {MealEntry} from "../meal_entry/mealEntrySchema";
-// 'mongodb+srv://messdb1:kibria7533@cluster0.ak8uw.mongodb.net/?retryWrites=true&w=majority'
-// mongodb://localhost/mess
-//mongodb+srv://rafi29:rafi4472@cluster0.gk0nw.mongodb.net/?retryWrites=true&w=majority
+
 @Module({
   imports: [ConfigModule.forRoot(),
-      MongooseModule.forRoot(process.env.DATABASE),
+    MongooseModule.forRoot(process.env.DATABASE),
     BazarListModule,
     MessModule,
     DepositModule,
@@ -26,11 +23,11 @@ import {MealEntry} from "../meal_entry/mealEntrySchema";
     RequestModule,
     MonthlyStatementModule,
     AuthModule,
-    MealEntryModule
+    MealEntryModule,
   ],
 
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
 export class AppModule {
 }
